5ed0797b35d7c5b009caa9b1b27f8a8f3ee97f6c
roman <roman@Romans-MacBook-Air-3.local>
2025-12-04 22:47:43 +0000

    plan workflow

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,7 +12,7 @@ terraform {
     key = "terraform.tfstate"
     region = "eu-west-2"
     use_lockfile = true #s3 native locking
-    profile      = "easyecs" 
+    # profile      = "easyecs" 
   }
 }
 
