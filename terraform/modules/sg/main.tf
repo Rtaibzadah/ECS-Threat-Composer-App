f2ddeefdd7bf5d94b5eae5f1b71c0500f7afd234
roman <roman@Romans-MacBook-Air-3.local>
2025-12-27 21:59:27 +0000

    updating workflows

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "alb_sg" {
 
 # ALB allows inbound HTTP 
 resource "aws_vpc_security_group_ingress_rule" "alb_http_80" {
-  description       = "SG, Allow HTTP for 80→443 redirect"
+  description       = "SG, Allow HTTP to ALB"
   security_group_id = aws_security_group.alb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
